8661fe8154ff7676aaedaf7d2bd2df4a1e901d7e
DynamicCake <128557765+DynamicCake@users.noreply.github.com>
2025-06-02 11:02:56 -1000

    Initial

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "ssg";
+
+  inputs = {nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";};
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    allSystems = ["x86_64-linux" "aarch64-darwin"];
+    # Found from
+    # https://nix4noobs.com/flakes/devshells/ # Dead link lol, I will provide an updated one soon
+    forAllSystems = fn:
+      nixpkgs.lib.genAttrs allSystems
+      (system: fn {pkgs = import nixpkgs {inherit system;};});
+  in {
+    devShells = forAllSystems ({pkgs}: {
+      default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+            mkdocs
+        ];
+      };
+    });
+  };
+}
